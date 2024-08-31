d9a8606117c5b9203ceda6c582e21afbf6e0286e
Albert <114226523+albert28928@users.noreply.github.com>
2024-08-31 16:25:49 +0800

    Add files via upload

---
+++ assets/js/fadeAnime.js
@@ -0,0 +1,267 @@
+function Contact_reveal() {
+    var reveals = document.querySelectorAll(".contact-item")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function title_reveal() {
+    var reveals = document.querySelectorAll(".h2.section-title")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function title_text_reveal() {
+    var reveals = document.querySelectorAll(".h1.section-text")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Members_reveal() {
+    var reveals = document.querySelectorAll(".members-list")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function About_reveal() {
+    var reveals = document.querySelectorAll(".about-card")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function News_reveal() {
+    var reveals = document.querySelectorAll(".News-list")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Recruit_reveal() {
+    var reveals = document.querySelectorAll(".RecruitBoard")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Hero_reveal() {
+    var reveals = document.querySelectorAll(".hero-content")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Project_title_reveal() {
+    var reveals = document.querySelectorAll(".h2.section-title")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Project_text_reveal() {
+    var reveals = document.querySelectorAll(".h1.section-text")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Project_reveal() {
+    var reveals = document.querySelectorAll(".projects-list")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Achievement_title_reveal() {
+    var reveals = document.querySelectorAll(".h2.section-title")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Achievement_text_reveal() {
+    var reveals = document.querySelectorAll(".h1.section-text")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Achievement_conference_reveal() {
+    var reveals = document.querySelectorAll(".Publications-Conference")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 100;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Achievement_journal_reveal() {
+    var reveals = document.querySelectorAll(".Publications-Journal")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 100;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Achievement_US_reveal() {
+    var reveals = document.querySelectorAll(".Patents-US")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 100;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function Achievement_TW_reveal() {
+    var reveals = document.querySelectorAll(".Patents-TW")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 50;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function LabPhoto_title_reveal() {
+    var reveals = document.querySelectorAll(".h2.section-title")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+function LabPhoto_text_reveal() {
+    var reveals = document.querySelectorAll(".h1.section-text")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+
+window.addEventListener("scroll", Contact_reveal);
+window.addEventListener("scroll", title_reveal);
+window.addEventListener("scroll", title_text_reveal);
+window.addEventListener("scroll", Members_reveal);
+window.addEventListener("scroll", About_reveal);
+window.addEventListener("scroll", News_reveal);
+window.addEventListener("scroll", Recruit_reveal);
+window.addEventListener("load", Hero_reveal);
+window.addEventListener("load", Project_title_reveal);
+window.addEventListener("load", Project_text_reveal);
+window.addEventListener("load", Project_reveal);
+window.addEventListener("load", Achievement_title_reveal);
+window.addEventListener("load", Achievement_text_reveal);
+window.addEventListener("load", Achievement_conference_reveal);
+window.addEventListener("scroll", Achievement_journal_reveal);
+window.addEventListener("scroll", Achievement_US_reveal);
+window.addEventListener("scroll", Achievement_TW_reveal);
+window.addEventListener("load", LabPhoto_title_reveal);
+window.addEventListener("load", LabPhoto_text_reveal);
